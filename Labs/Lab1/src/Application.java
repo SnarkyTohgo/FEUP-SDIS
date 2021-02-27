dbfd6e15f5282ac0ff8e826df292067f6214e8b9
SnarkyTohgo <tiago.rocha@affinity.pt>
2021-02-27 23:30:05 +0000

    implemented simple echo server

--- Labs/Lab1/src/Application.java
+++ Labs/Lab1/src/Application.java
@@ -1,5 +1,52 @@
-public class Application {
-    public static void main(String[] args) {
+import Client.Client;
+import Message.ClientRequestMessage;
+import Message.Message;
+import Server.Server;
 
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class Application {
+    static Client client;
+    static Server server;
+
+    public static void setup() throws SocketException, UnknownHostException {
+        client = new Client();
+
+        server = Server.getInstance(4445);
+        server.start();
+    }
+
+    public static void shutdown() throws IOException {
+        String op = "end";
+        ArrayList<String> params = new ArrayList<String>();
+        Message end = new ClientRequestMessage(op, params);
+
+        client.sendRequest(end);
+        client.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        setup();
+
+        String opRegister = "REGISTER";
+        ArrayList<String> paramsRegister = new ArrayList<String>();
+        paramsRegister.add("www.fe.up.pt");
+        paramsRegister.add("10.227.240.205");
+
+        Message register = new ClientRequestMessage(opRegister, paramsRegister);
+        String echo = client.sendRequest(register);
+        System.out.println(echo);
+
+//        String opLookup = "LOOKUP";
+//        ArrayList<String> paramsLookup = new ArrayList<String>();
+//        paramsLookup.add("www.fe.up.pt");
+//
+//        Message lookup = new ClientRequestMessage(opLookup, paramsLookup);
+//        client.sendRequest(lookup);
+
+        shutdown();
     }
 }
--- Labs/Lab1/src/Server/Server.java
+++ Labs/Lab1/src/Server/Server.java
@@ -7,7 +7,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class Server extends Thread {
@@ -46,12 +45,11 @@ public class Server extends Thread {
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
             String message = new String(packet.getData(), 0, packet.getLength());
-            // parse data and build reply
 
             packet = new DatagramPacket(buf, buf.length, address, port);
-            String received = new String(packet.getData(), 0, packet.getLength());
+            String request = new String(packet.getData(), 0, packet.getLength());
 
-            if (received.equals("end")) {
+            if (request.equals("end")) {
                 running = false;
                 continue;
             }
